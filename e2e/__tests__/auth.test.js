d7fac59593c530af1bf31f5f30efcee9c93d249f
allisonbusse <allisonbusse@gmail.com>
2019-10-11 09:42:10 -0700

    fixed auth test

--- e2e/__tests__/auth.test.js
+++ e2e/__tests__/auth.test.js
@@ -1,6 +1,8 @@
 const request = require('../request');
 const { dropCollection } = require('../db');
 const jwt = require('jsonwebtoken');
+const { signupUser } = require('../data-helpers');
+const User = require('../../lib/models/user');
 
 describe('Auth API', () => {
 
@@ -14,11 +16,8 @@ describe('Auth API', () => {
   let user = null;
 
   beforeEach(() => {
-    return request
-      .post('/api/auth/signup')
-      .send(testUser)
-      .expect(200)
-      .then(({ body }) => user = body);
+    return signupUser(testUser)
+      .then(newUser => user = newUser);
   });
 
   it('signs up a user', () => {
@@ -74,7 +73,7 @@ describe('Auth API', () => {
     });
   }
 
-  testBadSignup('rejects bad password', { 
+  testBadSignup('rejects bad password', {
     email: testUser.email,
     password: 'bad password'
   });
@@ -98,4 +97,113 @@ describe('Auth API', () => {
       .expect(401);
   });
 
+
+});
+
+describe('Auth Admin Users', () => {
+  const adminTest = {
+    email: 'alex@hellohello.com',
+    password: 'abc123'
+  };
+  const louslyOlUser = {
+    email: 'wassup@hellohello.com',
+    password: 'abc123'
+  };
+  function signinAdminUser(admin = adminTest) {
+    return request
+      .post('/api/auth/signin')
+      .send(admin)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+  it('allows admin to make changes to users', () => {
+    return signupUser(adminTest)
+      .then(user => {
+        return User.updateById(user._id,
+          {
+            $addToSet: {
+              roles: 'admin'
+            }
+          }
+        );
+      })
+      .then(() => {
+        return Promise.all([
+          signinAdminUser(),
+          signupUser(louslyOlUser)
+        ])
+          .then(([adminUser, user]) => {
+            return request
+              .put(`/api/auth/users/${user._id}/roles/admin`)
+              .set('Authorization', adminUser.token)
+              .expect(200)
+              .then(({ body }) => {
+                expect(body.roles[0]).toBe('admin');
+              });
+          });
+      });
+  });
+
+  const newUser = {
+    email: 'help@help.com',
+    password: 'abc'
+  };
+
+  it('allows admin to delete role', () => {
+    return Promise.all([
+      signinAdminUser(),
+      signupUser(newUser)
+    ])
+      .then(([adminUser, newUser]) => {
+        return request
+          .put(`/api/auth/users/${newUser._id}/roles/admin`)
+          .set('Authorization', adminUser.token)
+          .expect(200)
+          .then(({ body }) => {
+            return request
+              .delete(`/api/auth/users/${body._id}/roles/admin`)
+              .set('Authorization', adminUser.token)
+              .expect(200)
+              .then(({ body }) => {
+                expect(body.roles).toEqual([]);
+              });
+          });
+      });
+  });
+
+  it('gets all users', () => {
+    return Promise.all([
+      signinAdminUser(),
+      signupUser({
+        email: 'abbey@abbey.com',
+        password: 'abc'
+      }),
+      signupUser({
+        email: 'andy@andy.com',
+        password: '123'
+      })
+    ])
+      // eslint-disable-next-line no-unused-vars
+      .then(([adminUser, userOne, userTwo]) => {
+        return request
+          .put(`/api/auth/users/${userTwo._id}/roles/admin`)
+          .set('Authorization', adminUser.token)
+          .expect(200)
+          .then(() => {
+            return request
+              .get('/api/auth/users/')
+              .set('Authorization', adminUser.token)
+              .expect(200)
+              .then(({ body }) => {
+                expect(body.length).toEqual(6);
+                expect(body[5]).toEqual({
+                  email: 'andy@andy.com',
+                  roles: ['admin'],
+                  _id: expect.any(String)
+                });
+              });
+          });
+
+      });
+  });
 });
