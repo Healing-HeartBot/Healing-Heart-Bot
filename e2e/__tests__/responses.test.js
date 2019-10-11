ec45f44719f9a1d6cc15d1a90b866d1cb905ef81
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-11 14:43:57 -0700

    alex is lonely

--- e2e/__tests__/responses.test.js
+++ e2e/__tests__/responses.test.js
@@ -2,7 +2,6 @@ const request = require('../request');
 const Response = require('../../lib/models/response');
 
 describe('Response API', () => {
-
   beforeEach(() => {
     return Response.deleteMany({});
   });
@@ -13,23 +12,56 @@ describe('Response API', () => {
     moods: ['sad', 'bitter']
   };
 
+  const validResponse2 = {
+    content:
+      'https://open.spotify.com/track/44ADyYoY5liaRa3EOAl4uf?si=sm12QRuDQIm2Ku25LHJBIA',
+    type: 'song',
+    moods: ['sad', 'bitter']
+  };
+
+  const validResponse3 = {
+    content:
+      'https://open.spotify.com/track/7m8PEWF1Qjo4BmcjEYsacr?si=2IuxzCmsSfqX0WQ3S8qp0Q',
+    type: 'song',
+    moods: ['alex is lonely', 'sad']
+  };
+
+  const validResponse4 = {
+    content: 'response4',
+    type: 'song',
+    moods: ['sad', 'sad']
+  };
+
+  const validResponse5 = {
+    content: 'response5',
+    type: 'song',
+    moods: ['lonelyAlex', 'sad']
+  };
+
+  const validResponse6 = {
+    content: 'response6',
+    type: 'song',
+    moods: ['sad', 'weakAlex']
+  };
+
   function postResponse(response) {
-    return request
-      .post('/api/responses')
-      .send(response)
-      // .expect(200)
-      .then(({ body }) => body);
+    return (
+      request
+        .post('/api/responses')
+        .send(response)
+        // .expect(200)
+        .then(({ body }) => body)
+    );
   }
 
   it('it posts a valid response', () => {
-    return postResponse(validResponse)
-      .then(response => {
-        expect(response).toEqual({
-          ...validResponse,
-          _id: expect.any(String),
-          __v: 0
-        });
+    return postResponse(validResponse).then(response => {
+      expect(response).toEqual({
+        ...validResponse,
+        _id: expect.any(String),
+        __v: 0
       });
+    });
   });
 
   it('gets all valid responses', () => {
@@ -45,5 +77,35 @@ describe('Response API', () => {
             expect(body.length).toBe(1);
           });
       });
+  });
+
+  it('is going to get a random response by mood', () => {
+    return Promise.all([
+      postResponse(validResponse),
+      postResponse(validResponse2),
+      postResponse(validResponse3),
+      postResponse(validResponse4),
+      postResponse(validResponse5),
+      postResponse(validResponse6)
+    ]).then(() => {
+      return request
+        .get('/api/responses/sad')
+        .expect(200)
+        .then(({ body }) => {
+          console.log(body);
+          expect(body).toMatchInlineSnapshot(`
+            Object {
+              "__v": 0,
+              "_id": "5da0f793f3e1e258aef29c5e",
+              "content": "response5",
+              "moods": Array [
+                "lonelyAlex",
+                "sad",
+              ],
+              "type": "song",
+            }
+          `);
+        });
+    });
   });
 });
