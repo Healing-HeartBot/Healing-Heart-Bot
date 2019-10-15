f540add7a17e24c7bd012d71e1c74469fd5d8829
Pat Haynes <pathaynes32@gmail.com>
2019-10-15 10:55:07 -0700

    starting a new stream

--- bot.js
+++ bot.js
@@ -26,6 +26,21 @@ stream.on('tweet', function(tweet) {
     time: tweet.created_at,
     tweet: tweet.text
   };
+  console.log('hashtag', ent.hashtags);
+  console.log('tweet1', tweet.entities);
+  console.log('tweet2', tweet.text);
+
+  for(let i = 0; i < ent.hashtags.length; i++) {
+    if(ent.hashtags[i].text === 'staystrongbb') {
+      return request
+        .post('http://localhost:3000/api/responses')
+        .send({ content: userData.tweet })
+        .then(tweet => {
+          console.log(tweet);
+        });
+    }
+  }
+
 
   let tweetMood = ent.hashtags.map(mood => {
     return `moods=${mood.text}`;
@@ -34,19 +49,19 @@ stream.on('tweet', function(tweet) {
     tweetMood = tweetMood.join('&');
   }
 
-  return request
-    .get(`http://localhost:3000/api/responses/heartbot?${tweetMood}`)
-    .then(({ body }) => {
-      const mongoReq = new TwitterReq(userData);
-      const baseMoods = moodMapper(moods, tweetMood);
-      const base = baseMessages[baseMoods[0]];
-      console.log('this should be a tweet sent by user', mongoReq);
-      newTweeter.post('statuses/update', { status: `Hey @${user.screen_name}, ${base} ${body[0].content}` }, function(err, data) {
-        console.log('bot tweet', data);
-      });
-    })
-    .catch(error => {
-      console.log(error);
-    });
+  // return request
+  //   .get(`http://localhost:3000/api/responses/heartbot?${tweetMood}`)
+  //   .then(({ body }) => {
+  //     const mongoReq = new TwitterReq(userData);
+  //     const baseMoods = moodMapper(moods, tweetMood);
+  //     const base = baseMessages[baseMoods[0]];
+  //     console.log('this should be a tweet sent by user', mongoReq);
+  //     newTweeter.post('statuses/update', { status: `Hey @${user.screen_name}, ${base} ${body[0].content}` }, function(err, data) {
+  //       console.log('bot tweet', data);
+  //     });
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   });
 
 });
