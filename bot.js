1edff04400edac4a0795304cba5930de5fa96568
adoxic <griffinmaeve@gmail.com>
2019-10-15 09:27:38 -0700

    eslint

--- bot.js
+++ bot.js
@@ -42,7 +42,7 @@ stream.on('tweet', function(tweet) {
       console.log(baseMoods);
       const base = baseMessages[baseMoods[0]];
       console.log('this should be a tweet sent by user', mongoReq);
-      newTweeter.post('statuses/update', { status: `Hey @${user.screen_name}, ${base} ${body[0].content}` }, function(err, data, response) {
+      newTweeter.post('statuses/update', { status: `Hey @${user.screen_name}, ${base} ${body[0].content}` }, function(err, data) {
       
         console.log('bot tweet', data);
       });
