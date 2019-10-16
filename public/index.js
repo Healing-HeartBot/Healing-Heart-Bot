e73fe3cb6ada10f36a3861586b328fdda10c17ae
Pat Haynes <pathaynes32@gmail.com>
2019-10-16 16:34:12 -0700

    animations

--- public/index.js
+++ public/index.js
@@ -1,11 +1,23 @@
 import { getResponse, getGiphy } from './util/helper-functions.js';
 const form = document.getElementById('form')
 const response = document.getElementById('response');
+const logo = document.getElementById('logo')
+const head = document.getElementById('robot-head')
+const right = document.getElementById('robot-right')
+const left = document.getElementById('robot-left')
+const feet = document.getElementById('robot-feet')
 // const textBox = document.getElementById('text-box')
 
 form.addEventListener('submit', event => {
+  setTimeout(() => {
+    head.classList.remove('hidden');
+    right.classList.remove('hidden');
+    left.classList.remove('hidden');
+    feet.classList.remove('hidden');
+  }, 300);
+  logo.classList.add('animate');
   event.preventDefault();
-  while(response.hasChildNodes()) {
+  while (response.hasChildNodes()) {
     response.removeChild(response.firstChild);
   }
   const mood = form.dropdown.value;
@@ -13,7 +25,7 @@ form.addEventListener('submit', event => {
     .then(receivedResponse => {
       const botResponse = receivedResponse[0].content;
       console.log(botResponse);
-      if(botResponse.includes('giphy')) {
+      if (botResponse.includes('giphy')) {
         const giphyId = botResponse.slice(23, botResponse.length - 1);
         getGiphy(giphyId)
           .then(gifData => {
@@ -25,7 +37,7 @@ form.addEventListener('submit', event => {
             response.append(giphyImage);
           })
       }
-      else if(botResponse.includes('spotify')){
+      else if (botResponse.includes('spotify')) {
         const spotifyPlayer = document.createElement('iframe');
         spotifyPlayer.src = botResponse.slice(0, 25) + 'embed/' + botResponse.slice(25);
         spotifyPlayer.width = 300;
