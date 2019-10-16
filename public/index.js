46c48c2fc7c102d2c6d10dfa2ab5e7dbf4e91a75
Alex Spencer <alexander.j.spencer@gmail.com>
2019-10-16 15:03:24 -0700

    front end stuff

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 import { getResponse, getGiphy } from './util/helper-functions.js';
 const form = document.getElementById('form')
 const response = document.getElementById('response');
+// const textBox = document.getElementById('text-box')
 
 form.addEventListener('submit', event => {
   event.preventDefault();
@@ -13,17 +14,18 @@ form.addEventListener('submit', event => {
       const botResponse = receivedResponse[0].content;
       console.log(botResponse);
       if(botResponse.includes('giphy')) {
-        const giphyId = botResponse.slice(23, botResponse.length);
+        const giphyId = botResponse.slice(23, botResponse.length - 1);
         getGiphy(giphyId)
           .then(gifData => {
             const giphyImage = document.createElement('iframe');
             giphyImage.src = gifData.data.embed_url;
-            giphyImage.width = gifData.data.images.original.width;
-            giphyImage.height = gifData.data.images.original.height;
+            giphyImage.border = 0;
+            giphyImage.width = gifData.data.images.downsized_small.width;
+            giphyImage.height = gifData.data.images.downsized_small.height;
             response.append(giphyImage);
           })
       }
-      if(botResponse.includes('spotify')){
+      else if(botResponse.includes('spotify')){
         const spotifyPlayer = document.createElement('iframe');
         spotifyPlayer.src = botResponse.slice(0, 25) + 'embed/' + botResponse.slice(25);
         spotifyPlayer.width = 300;
@@ -35,9 +37,12 @@ form.addEventListener('submit', event => {
         response.append(spotifyPlayer);
       }
       else {
+        const textBox = document.createElement('div')
+        textBox.classList.add('text-box');
         const p = document.createElement('p');
         p.innerHTML = botResponse;
-        response.append(p);
+        textBox.append(p);
+        response.append(textBox);
       }
     })
 })
