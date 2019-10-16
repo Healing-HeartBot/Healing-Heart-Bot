bcdf0020e19058facc9c1386eb035984fc8f3ffb
Abbey Masters <abbeykmasters@gmail.com>
2019-10-16 11:12:19 -0700

    trying to fix error

--- bot.js
+++ bot.js
@@ -28,7 +28,7 @@ stream.on('tweet', function(tweet) {
     tweet: tweet.text
   };
 
-  postTwitReq(userData);
+  // postTwitReq(userData);
 
   const parseTweet = tweet.text.replace(/([@#][\w_-]+)\s/gi, '');
   const tweetId = tweet.id_str;
@@ -83,9 +83,9 @@ stream.on('tweet', function(tweet) {
   }
 });
 
-function postTwitReq(newTweet) {
-  return request
-    .post(`${process.env.BASE_URL}/api/twitreq`)
-    .send(newTweet)
-    .then(({ body }) => body);
-}
+// function postTwitReq(newTweet) {
+//   return request
+//     .post(`${process.env.BASE_URL}/api/twitreq`)
+//     .send(newTweet)
+//     .then(({ body }) => body);
+// }
